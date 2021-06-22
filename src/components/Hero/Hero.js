7bc581273888d2c52b4b4bc21a8808dbe0be9386
mcjovial <emmanuel.elias.232317@unn.edu.ng>
2021-06-22 19:12:45 -0400

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Here, I am <br/> Elias Emmanuel Atyusan
+      </SectionTitle>
+      <SectionText>
+        {/* <code> */}
+          I am a <code style={{color: 'white'}}>Software Developer</code>, with over 3 years of experience in Web Development. <code style={{color: 'white'}}>Experienced</code> with all stages of the development cycle for dynamic web projects.
+        {/* </code> */}
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Hire Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
